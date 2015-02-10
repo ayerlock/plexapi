3acb3fac9bee866c2b9846fbdfd4bbcd2daffff0
Michael J. Ayers <ayersm@gmail.com>
2015-02-09 23:27:53 -0500

    Modified default location of log file to be current directory.

--- plexapi/__init__.py
+++ plexapi/__init__.py
@@ -16,7 +16,7 @@ X_PLEX_PROVIDES = 'controller'                      # one or more of [player, co
 X_PLEX_PRODUCT = PROJECT                            # Plex application name, eg Laika, Plex Media Server, Media Link
 X_PLEX_VERSION = VERSION                            # Plex application version number
 X_PLEX_DEVICE = platform.platform()                 # Device name and model number, eg iPhone3,2, Motorola XOOM, LG5200TV
-X_PLEX_IDENTIFIER = str(hex(getnode()))             # UUID, serial number, or other number unique per device
+X_PLEX_IDENTIFIER = str( hex( getnode() ) )         # UUID, serial number, or other number unique per device
 BASE_HEADERS = {
     'X-Plex-Platform': X_PLEX_PLATFORM,
     'X-Plex-Platform-Version': X_PLEX_PLATFORM_VERSION,
@@ -28,10 +28,10 @@ BASE_HEADERS = {
 }
 
 # Logging Configuration
-log = logging.getLogger('plexapi')
-logfile = os.path.join('/tmp', 'plexapi.log')
-logformat = logging.Formatter('%(asctime)s %(module)-12s %(levelname)-6s %(message)s')
-filehandler = RotatingFileHandler(logfile, 'a', 512000, 3)
-filehandler.setFormatter(logformat)
-log.addHandler(filehandler)
-log.setLevel(logging.INFO)
+log = logging.getLogger( 'plexapi' )
+logfile = ( 'plexapi.log' )
+logformat = logging.Formatter( '%(asctime)s %(module)-12s %(levelname)-6s %(message)s' )
+filehandler = RotatingFileHandler( logfile, 'a', 512000, 3 )
+filehandler.setFormatter( logformat )
+log.addHandler( filehandler )
+log.setLevel( logging.INFO )
